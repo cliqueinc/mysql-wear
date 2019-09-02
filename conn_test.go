177e50d7aa2180863683e3f2ceda272d9cd008f1
Vladimir Kovpak <cn007b@gmail.com>
2019-09-02 14:35:53 +0300

    Updated the way to create test db.

--- conn_test.go
+++ conn_test.go
@@ -5,16 +5,15 @@ import (
 	"flag"
 	"fmt"
 	"os"
-	"os/exec"
-	"strconv"
 	"testing"
 	"time"
 )
 
 const (
+	TestHost     = "127.0.0.1"
 	TestPort     = 3306
-	TestUser     = "root"
-	TestPassword = "testpass1234"
+	TestUser     = "root"         // root user
+	TestPassword = "testpass1234" // root password
 )
 
 var db *DB
@@ -28,6 +27,19 @@ func TestMain(m *testing.M) {
 	envDBName := "mw_tmp" // See **Note** below. CreateDB will set this
 	// debugEnabled = true
 
+	// TODO combine with core way of connecting to mysql so we don't forget to change this in two places
+	dsn := fmt.Sprintf(
+		"%s:%s@tcp(%s:%d)/?tls=false&parseTime=true&charset=utf8mb4&collation=utf8mb4_unicode_ci&sql_mode=''",
+		TestUser,
+		TestPassword,
+		TestHost,
+		TestPort,
+	)
+	mysqlDB, err := sql.Open("mysql", dsn)
+	if err != nil {
+		panic(err)
+	}
+
 	var shouldDropDB bool
 	// **Note** This logic pre-dates the current version of mysql-wear!
 	// What this is doing is making sure your configured dbname (which is SUPPOSED
@@ -38,28 +50,20 @@ func TestMain(m *testing.M) {
 	// if !strings.HasPrefix(envDBName, "mw_tmp") {
 	if true {
 		// Create and new a tmp db
-		envDBName = CreateDB("mw_tmp")
+		envDBName = CreateDB(mysqlDB, "mw_tmp")
 		shouldDropDB = true
 
 	}
 
-	// TODO combine with core way of connecting to mysql so we don't forget to change this in two places
-	dsn := fmt.Sprintf(
-		"%s:%s@tcp(127.0.0.1:%d)/%s?tls=false&parseTime=true&charset=utf8mb4&collation=utf8mb4_unicode_ci&sql_mode=''",
-		TestUser,
-		TestPassword,
-		TestPort,
-		envDBName,
-	)
-	mysqlDB, err := sql.Open("mysql", dsn)
-	if err != nil {
-		panic(err)
+	if _, err := mysqlDB.Exec("USE " + envDBName); err != nil {
+		panic(fmt.Sprintf("Failed to switch DB, error (%s)\n", err))
 	}
+
 	db = New(mysqlDB)
 
 	exitVal := m.Run()
 	if shouldDropDB {
-		DropDB(envDBName)
+		DropDB(mysqlDB, envDBName)
 	}
 	mysqlDB.Close()
 	os.Exit(exitVal)
@@ -68,42 +72,24 @@ func TestMain(m *testing.M) {
 /*
 These should only be used during local testing
 */
-func CreateDB(namePrefix string) string {
+func CreateDB(mysqlDB *sql.DB, namePrefix string) string {
 	t := time.Now()
-	dbName := fmt.Sprintf("%s_%s", namePrefix, t.Format("2006-01-02-15:04:05"))
-	// Create the tmp db, make sure can connect to its
-	out, err := exec.Command(
-		"mysqladmin",
-		"-h127.0.0.1",
-		"-P"+strconv.Itoa(TestPort),
-		"-u"+TestUser,
-		"-p"+TestPassword,
-		"create",
-		dbName,
-	).CombinedOutput()
+	dbName := fmt.Sprintf("%s_%s", namePrefix, t.Format("2006_01_02_15_04_05"))
 
+	_, err := mysqlDB.Exec("CREATE DATABASE " + dbName)
 	if err != nil {
-		panic(fmt.Sprintf("Failed to created temporary DB, Output: (%s) err (%s)\n", out, err))
+		panic(fmt.Sprintf("Failed to created temporary DB, error (%s)\n", err))
 	}
 
 	fmt.Println("Created new tmp testing db", dbName)
 	return dbName
 }
 
-func DropDB(dbName string) {
-	out, err := exec.Command(
-		"mysqladmin",
-		"-f", // Need the -f to force drop db otherwise will prompt y/n
-		"-h127.0.0.1",
-		"-P"+strconv.Itoa(TestPort),
-		"-u"+TestUser,
-		"-p"+TestPassword,
-		"drop",
-		dbName,
-	).CombinedOutput()
+func DropDB(mysqlDB *sql.DB, dbName string) {
+	_, err := mysqlDB.Exec("DROP DATABASE " + dbName)
 
 	if err != nil {
-		panic(fmt.Sprintf("Failed to drop temporary DB, Output: (%s) err (%s)\n", out, err))
+		panic(fmt.Sprintf("Failed to drop temporary DB, error (%s)\n", err))
 	}
 	fmt.Println("Dumped testing db", dbName)
 }
